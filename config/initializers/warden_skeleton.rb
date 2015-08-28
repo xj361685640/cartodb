36591fe26da77c97ab9063e6cb35ba4f043fc0d0
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2015-08-28 14:16:11 +0200

    SAML header authentication example

--- config/initializers/warden_skeleton.rb
+++ config/initializers/warden_skeleton.rb
@@ -1,15 +1,14 @@
-# This sample strategy shows how can you get request headers and parameters. It will automatically log in based on subdomain. Example: http://myuser.localhost.lan:3000/dashboard/maps will login "myuser" user and display dashboard map page.
-# This is automatically available because of initializer directory. You can invoke it at ApplicationController so every request uses it:
+# This sample strategy shows how can you get request headers and parameters, and login the user from a trusted header (method to be completed).
+# Here's a sample usage from a controller method (also works foor subdomainless, extract_subdomain takes care of it):
 #
-#  before_filter :dummy_authentication
-#
-#  def dummy_authentication
+#  def login_with_saml
 #    subdomain = CartoDB.extract_subdomain(request)
-#    authenticate!(:dummy, {}, :scope => subdomain) unless authenticated?(subdomain)
+#    authenticate!(:saml_header, :scope => subdomain) unless authenticated?(subdomain)
+#    redirect_to CartoDB.path(self, 'dashboard', {trailing_slash: true})
 #  end
 
 
-Warden::Strategies.add(:dummy) do
+Warden::Strategies.add(:saml_header) do
 
   def valid?
     true
@@ -20,11 +19,12 @@ Warden::Strategies.add(:dummy) do
   end
 
   def authenticate!
-    raise "Headers not available" if request.headers.nil?
-    raise "Params not available" if params.nil?
-    username_from_subdomain = request.host.split('.')[0]
-    user = User.where(username: username_from_subdomain).first
-    success!(user)
+    user = User.where(username: username_from_saml).first
+    user ? success!(user) : fail!
+  end
+
+  def username_from_saml
+    // TODO: extract the username from request.headers or params
   end
 
 end
