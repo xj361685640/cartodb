be3e466781e306bab9c04b50525a405e19e58f04
Kartones <kartones@cartodb.com>
2015-10-30 11:21:18 +0100

    #6002 checking permissions and not ownership for overlays show action

--- app/controllers/carto/api/overlays_controller.rb
+++ app/controllers/carto/api/overlays_controller.rb
@@ -8,13 +8,12 @@ module Carto
       include Carto::UUIDHelper
 
       ssl_required :index, :show
-      before_filter :check_current_user_has_permissions_on_vis, only: [ :index ]
-      before_filter :check_current_user_owns_vis, only: [ :show ]
+      before_filter :check_current_user_has_permissions_on_vis, only: [:index, :show]
 
       def index
-        collection = Carto::Overlay.where(visualization_id: params.fetch('visualization_id')).map { |overlay|
+        collection = Carto::Overlay.where(visualization_id: params.fetch('visualization_id')).map do |overlay|
           Carto::Api::OverlayPresenter.new(overlay).to_poro
-        }
+        end
         render_jsonp(collection)
       rescue KeyError
         head :not_found
@@ -29,25 +28,26 @@ module Carto
 
       protected
 
+      # This method is not currently used, but will be when the remainder of api/json actions come here
       def check_current_user_owns_vis
-        head 401 and return if current_user.nil?
-        head 401 and return unless is_uuid?(params.fetch('id'))
+        head 401 && return if current_user.nil?
+        head 401 && return unless is_uuid?(params.fetch('id'))
 
         overlay = Carto::Overlay.where(id: params.fetch('id')).first
-        head 401 and return if overlay.nil?
+        head 401 && return if overlay.nil?
 
         vis = Carto::Visualization.where(id: overlay.visualization_id).first
-        head 403 and return if vis.user_id != current_user.id
+        head 403 && return if vis.user_id != current_user.id
       end
 
       def check_current_user_has_permissions_on_vis
-        head 401 and return if current_user.nil?
-        head 401 and return unless is_uuid?(params.fetch('visualization_id'))
+        head 401 && return if current_user.nil?
+        head 401 && return unless is_uuid?(params.fetch('visualization_id'))
 
         vis = Carto::Visualization.where(id: params.fetch('visualization_id')).first
-        head 401 and return if vis.nil?
+        head 401 && return if vis.nil?
 
-        head 403 and return if !vis.is_writable_by_user(current_user)
+        head 403 && return if !vis.is_writable_by_user(current_user)
       end
 
     end
