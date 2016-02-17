ac3a0b87cb934fe055f624bb96ad2874e9a5634c
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-02-17 16:54:12 +0100

    Adds zeus for rake tasks as well

--- ci.rb
+++ ci.rb
@@ -1,9 +1,9 @@
 start_time = Time.now
 
-puts "Hello! It's #{start_time}"
+puts "Hello! It's #{start_time} and CI is just starting!"
 
 `make check`
 
-puts "Task finished in #{Time.now - start_time}"
+puts "CI finished in #{Time.now - start_time}"
 
 puts 'Bye!'
