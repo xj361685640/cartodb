d6c3bb54ab96e7d225c2b8b6347fa0301125aa7b
Román Jiménez <roman@cartodb.com>
2017-08-24 13:37:44 +0200

    back to webpack

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,78 @@
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine-ajax', 'jasmine'],
     files: [
-      '.grunt/main.affected-specs.js'
+      './lib/assets/core/test/spec/cartodb3/loadtests.js'
     ],
+    preprocessors: {
+      './lib/assets/core/test/spec/cartodb3/loadtests.js': ['webpack']
+    },
+    webpack: {
+      entry: () => {
+        return {};
+      }, // For webpack 2.2.0 and beyond, config schema is strictly enforced and will error if it's missing something required.
+      devtool: 'cheap-module-source-map',
+      plugins: [
+        new webpack.ProvidePlugin({
+          $: 'jquery',
+          jQuery: 'jquery',
+          ['window.jQuery']: 'jquery'
+        })
+      ],
+      module: {
+        rules: [{
+          test: /\.js$/,
+          loader: 'shim-loader',
+          query: {
+            shim: {
+              'wax.cartodb.js': {
+                exports: 'wax'
+              },
+              'html-css-sanitizer': {
+                exports: 'html'
+              }
+            }
+          }
+        }, {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          include: [path.resolve(path.resolve('.'), 'node_modules/tangram.cartodb')],
+          options: {
+            presets: [
+              ['es2015', {
+                'modules': false
+              }]
+            ]
+          }
+        }, {
+          test: /\.tpl$/,
+          use: 'tpl-loader'
+        }, {
+          test: /\.mustache$/,
+          use: 'raw-loader'
+        }],
+        exprContextRegExp: /$^/,
+        exprContextCritical: false
+      },
+      node: {
+        fs: 'empty' // This fixes the error Module not found: Error: Can't resolve 'fs'
+      },
+      stats: 'none',
+      performance: {
+        hints: false
+      }
+    },
+    webpackMiddleWare: {
+      stats: 'errors-only'
+    },
     reporters: ['dots'],
+    reportSlowerThan: 1000,
     autoWatch: true,
+    autoWatchBatchDelay: 500,
     restartOnFileChange: true,
     port: 9876,
     colors: true,
